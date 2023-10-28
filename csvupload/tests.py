fa531bd84d5d7705cc5ad2e187cd6925ba685070
Robert Edwardes <rdedwardes@gmail.com>
2023-10-28 07:16:30 -0400

    basic login browser test

--- csvupload/tests.py
+++ csvupload/tests.py
@@ -1,6 +1,11 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
-from csvupload.models import main_table, label_group
+from django.contrib.staticfiles.testing import LiveServerTestCase
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+from csvupload.models import main_table, label_group, label_tag
 
 class DataBaseTest(TestCase):
     def test_insert_json_into_database(self):
@@ -15,12 +20,23 @@ class DataBaseTest(TestCase):
 
     def test_insert_label_str_into_database(self):
         label_str = 'label_test'
+
         my_model_instance = label_group(label=label_str)
         my_model_instance.save()
 
         saved_instance = label_group.objects.get(id=my_model_instance.id)
 
         self.assertEqual(saved_instance.label, label_str)
+
+    def test_insert_tag_str_into_database(self):
+        tag_str = 'tag_test'
+
+        my_model_instance = label_tag(label=tag_str)
+        my_model_instance.save()
+
+        saved_instance = label_tag.objects.get(id=my_model_instance.id)
+
+        self.assertEqual(saved_instance.label, tag_str)
 
 class FileUploadTest(TestCase):
     def setUp(self):
@@ -36,4 +52,25 @@ class FileUploadTest(TestCase):
         with open('test_data.csv', 'rb') as fp:
             response = c.post("/admin/csvupload/upload-csv/",{"csv_upload": fp}, follow=True)
 
-        self.assertEqual(response.status_code, 200) 
+        self.assertEqual(response.status_code, 200)
+
+class LiveUiTesting(LiveServerTestCase):
+    fixtures = ['user-data.json']
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+    
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        self.selenium.get(f"{self.live_server_url}/admin/")
+        username_input = self.selenium.find_element(By.NAME, "username")
+        username_input.send_keys("myuser")
+        password_input = self.selenium.find_element(By.NAME, "password")
+        password_input.send_keys("secret")
+        self.selenium.find_element(By.XPATH, '//input[@value="Log in"]').click()
