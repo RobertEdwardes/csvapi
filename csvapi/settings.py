4abb82ce363d775f261ace60961e71b204ef9d33
Robert Edwardes <rdedwardes@gmail.com>
2023-12-18 10:33:50 -0500

    Update settings.py

--- csvapi/settings.py
+++ csvapi/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ftaxutfq_503u)pg!ic4korvpth+hxq5ajl58x#xag+9+78mih'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
